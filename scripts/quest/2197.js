b118f293799a10ab6e29d72697521e4ce0b41867
CCasusensa <bishoptate01@gmail.com>
2015-09-15 20:32:16 +0800

    quest chinese

--- scripts/quest/2197.js
+++ scripts/quest/2197.js
@@ -1,12 +1,12 @@
-var status = -1;
+﻿var status = -1;
 
 function start(mode, type, selection) {
-	qm.sendNext("Oh, you already have monster book.");
+	qm.sendNext("哦，你已經有了怪物的書。");
 	qm.forceCompleteQuest();
 	qm.dispose();
 }
 function end(mode, type, selection) {
-	qm.sendNext("Oh, you already have monster book.");
+	qm.sendNext("哦，你已經有了怪物的書。");
 	qm.forceCompleteQuest();
 	qm.dispose();
 }
