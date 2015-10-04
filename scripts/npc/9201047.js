f43c3cd8ec7d02d3245c78e6016ecf63168a985b
CCasusensa <bishoptate01@gmail.com>
2015-10-04 15:16:55 +0800

    npc chnage

--- scripts/npc/9201047.js
+++ scripts/npc/9201047.js
@@ -1,8 +1,8 @@
-var status = -1;
+﻿var status = -1;
 
 function action(mode, type, selection) {
     if (mode == 1) {
-	status++;
+		status++;
     } else {
 	if (status == 0) {
 	    cm.dispose();
@@ -10,23 +10,23 @@ function action(mode, type, selection) {
 	status--;
     }
     if (cm.getPlayer().getMapId() == 670011000) {
-	cm.gainNX(2000);
-	cm.warp(670010000,0);
-	cm.dispose();
+		//cm.gainNX(2000);
+		cm.warp(670010000,0);
+		cm.dispose();
 	return;
     }
     var em = cm.getEventManager("Amoria");
     if (em == null || !cm.isLeader()) {
-	cm.dispose();
-	return;
+		cm.dispose();
+		return;
     }
     if (em.getProperty("apq1").equals("0")) {
 	if (cm.getMap().getAllMonstersThreadsafe().size() == 0) {
 	    em.setProperty("apq1", "1");
-	    cm.mapMessage(5, "The Magik Fiarry has been spawned somewhere in the map.");
+	    cm.mapMessage(5, "某種東西已經在地圖上某個地方生成了。");
 	    cm.spawnMonster(9400518,1);
 	} else {
-	    cm.sendOk("Do me a favour and eliminate all the monsters, would you?");
+	    cm.sendOk("請問你可以幫我消除地圖上所有的怪物嗎？？");
 	}
     } else if (em.getProperty("apq1").equals("1")) {
 	if (cm.haveItem(4031595)) {
@@ -35,8 +35,8 @@ function action(mode, type, selection) {
     	    cm.playSound(true, "Party1/Clear");
 	    em.setProperty("apq1", "2");
 	} else {
-	    cm.sendOk("Use the Wing Hammer from the Fiarry to break the Mirror, and show me proof.");
-	}
+	    cm.sendOk("請告訴我使用永錘從輪渡打破鏡子的證明。");
+		}
     }
     cm.dispose();
 }
