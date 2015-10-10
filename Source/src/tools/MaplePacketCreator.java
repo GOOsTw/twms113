99d3940399fc3df0aaee5745a4de2bcb1f03dc0f
Weber Tsai <taida957789@gmail.com>
2015-10-10 20:49:59 +0800

    fix

--- Source/src/tools/MaplePacketCreator.java
+++ Source/src/tools/MaplePacketCreator.java
@@ -11,7 +11,7 @@
  GNU Affero General Public License.
 
  This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
+ but WITHOUT ANY WARRANTY; without even the implied warranty of 
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Affero General Public License for more details.
 
