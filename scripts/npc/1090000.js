80456ef649466139b3885de576b80c49dd45ed73
CCasusensa <bishoptate01@gmail.com>
2015-10-25 14:17:54 +0800

    fixed quest

--- scripts/npc/1090000.js
+++ scripts/npc/1090000.js
@@ -60,8 +60,11 @@ function action(mode, type, selection) {
 		}
 		cm.dispose();
 		} else if (cm.isQuestActive(6370)) {
-		cm.forceCompleteQuest(6370);
-		cm.dispose();
+			cm.forceCompleteQuest(6370);
+			cm.dispose();
+		} else if (cm.isQuestActive(6330)) {
+			cm.forceCompleteQuest(6330);
+			cm.dispose();		
 	    } else {
 		cm.sendOk("你好,我是卡伊琳-海盜轉職官.");
 		cm.dispose();
