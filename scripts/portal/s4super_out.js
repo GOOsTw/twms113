129972edbd7936af7bbac822418b8507e068a079
CCasusensa <bishoptate01@gmail.com>
2015-10-28 20:11:08 +0800

    change quest portal

--- scripts/portal/s4super_out.js
+++ scripts/portal/s4super_out.js
@@ -8,6 +8,7 @@ function enter(pi) {
 	if (pt.getInstance("KyrinTrainingGroundV").getTimeLeft() < 120000) { // 2 minutes left
 		pi.gainItem(4031059, 1);
 	    pi.warp(912010200, 0);
+		return true;
 	} else {
 	    pi.playerMessage("必須再忍耐一下子!");
 	    return false;
