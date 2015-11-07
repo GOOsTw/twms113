3b4e65ee67d52d4ced87783deb311ed649eb6fdb
Weber Tsai <taida957789@gmail.com>
2015-11-07 13:38:27 +0900

    fix reactor

--- scripts/reactor/2008006.js
+++ scripts/reactor/2008006.js
@@ -3,7 +3,7 @@ function act() {
     if(rm != null)
         rm.changeMusic("Bgm02/MissingYou");
     var em = rm.getEventManager("OrbisPQ");
-    angeMusicf (em != null) {
+    if (em != null) {
         em.setProperty("stage3", "1");
     }
 }
