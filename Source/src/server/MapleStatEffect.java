9914373adfdaa5c0b863262f67493afc0f7e8ad8
CCasusensa <bishoptate01@gmail.com>
2015-11-08 10:12:03 +0800

    fix Nautilus can't use town scroll

--- Source/src/server/MapleStatEffect.java
+++ Source/src/server/MapleStatEffect.java
@@ -66,8 +66,8 @@ public class MapleStatEffect implements Serializable {
         return loadFromData(source, itemid, false, false, (byte) 1);
     }
 
-    private static void addBuffStatPairToListIfNotZero(final List<Pair<MapleBuffStat, Integer>> list, final MapleBuffStat buffstat, final Integer val) {
-        if (val != 0) {
+    private static final void addBuffStatPairToListIfNotZero(final List<Pair<MapleBuffStat, Integer>> list, final MapleBuffStat buffstat, final Integer val) {
+        if (val.intValue() != 0) {
             list.add(new Pair<>(buffstat, val));
         }
     }
@@ -870,22 +870,29 @@ public class MapleStatEffect implements Serializable {
 
     public final boolean applyReturnScroll(final MapleCharacter applyto) {
         if (moveTo != -1) {
-           // if (applyto.getMap().getReturnMapId() != applyto.getMapId()) {
-                MapleMap target;
-                if (moveTo == 999999999) {
-                    target = applyto.getMap().getReturnMap();
-                } else {
-                    target = ChannelServer.getInstance(applyto.getClient().getChannel()).getMapFactory().getMap(moveTo);
-                    if (target.getId() / 10000000 != 60 && applyto.getMapId() / 10000000 != 61) {
-                        if (target.getId() / 10000000 != 21 && applyto.getMapId() / 10000000 != 20) {
-                            if (target.getId() / 10000000 != applyto.getMapId() / 10000000) {
-                                return false;
+            // if (applyto.getMap().getReturnMapId() != applyto.getMapId()) {
+            MapleMap target;
+            if (moveTo == 999999999) {
+                target = applyto.getMap().getReturnMap();
+            } else {
+                target = ChannelServer.getInstance(applyto.getClient().getChannel()).getMapFactory().getMap(moveTo);
+                if (target.getId() / 10000000 != 60 && applyto.getMapId() / 10000000 != 61) {
+                    if (target.getId() / 10000000 != 21 && applyto.getMapId() / 10000000 != 20) {
+                        if (target.getId() / 10000000 != applyto.getMapId() / 10000000) {
+                            if (target.getId() == 120000000 && applyto.getMapId() != 120000000) {
+                                applyto.changeMap(target, target.getPortal(0));
+                                return true;
+                            } else if (target.getId() != 120000000 && applyto.getMapId() == 120000000) {
+                                applyto.changeMap(target, target.getPortal(0));
+                                return true;
                             }
+                            return false;
                         }
                     }
                 }
-                applyto.changeMap(target, target.getPortal(0));
-                return true;
+            }
+            applyto.changeMap(target, target.getPortal(0));
+            return true;
             //}
         }
         return false;
@@ -1053,8 +1060,8 @@ public class MapleStatEffect implements Serializable {
         applyto.getClient().sendPacket(MaplePacketCreator.giveBuff(sourceid, 99999, stat, this)); // Hackish timing, todo find out
 
         final long starttime = System.currentTimeMillis();
-	final CancelEffectAction cancelAction = new CancelEffectAction(applyto, this, starttime);
-	final ScheduledFuture<?> schedule = Timer.BuffTimer.getInstance().schedule(cancelAction, ((starttime + 99999) - System.currentTimeMillis()));
+        final CancelEffectAction cancelAction = new CancelEffectAction(applyto, this, starttime);
+        final ScheduledFuture<?> schedule = Timer.BuffTimer.getInstance().schedule(cancelAction, ((starttime + 99999) - System.currentTimeMillis()));
         applyto.registerEffect(this, starttime, null);
     }
 
@@ -1639,6 +1646,9 @@ public class MapleStatEffect implements Serializable {
         }
         return false;
     }
+    /*public boolean isPoison() {
+     return skill && (sourceid == 2111003 || sourceid == 4221006 || sourceid == 12111005 || sourceid == 14111006 || sourceid == 22161003 || sourceid == 32121006 || sourceid == 1076 || sourceid == 11076 || sourceid == 4121015); // poison mist, smokescreen and flame gear, recovery aura
+     }*/
 
     private boolean isMist() {
         return skill && (sourceid == 2111003 || sourceid == 4221006 || sourceid == 12111005 || sourceid == 14111006 || sourceid == 22161003); // poison mist, smokescreen and flame gear, recovery aura
