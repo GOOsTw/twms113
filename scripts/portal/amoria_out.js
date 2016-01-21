0c98fadc5709a2a86c0d1a15fcc297d41e59de96
Weber Tsai <taida957789@gmail.com>
2016-01-21 17:42:56 +0800

    fix portal

--- scripts/portal/amoria_out.js
+++ scripts/portal/amoria_out.js
@@ -1,5 +1,8 @@
 function enter(pi) {
     pi.playPortalSE();
-    pi.warp(pi.getSavedLocation("AMORIA"), 0);
+    var map = pi.getSavedLocation("AMORIA");
+    if(map < 0)
+        map = 100000000;
+    pi.warp(map, 0);
     pi.clearSavedLocation("AMORIA");
 }
