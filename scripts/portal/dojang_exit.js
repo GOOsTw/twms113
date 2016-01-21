33a574e7d425d0a1fa3d902a975d3b1d15235c40
Weber Tsai <taida957789@gmail.com>
2016-01-21 14:29:40 +0800

    fix exit portal

--- scripts/portal/dojang_exit.js
+++ scripts/portal/dojang_exit.js
@@ -1,5 +1,8 @@
 function enter(pi) {
     pi.playPortalSE();
-    pi.warp(pi.getSavedLocation("MULUNG_TC"), 0);
+    var map = pi.getSavedLocation("MULUNG_TC");
+    if( map == -1 )
+        map = 100000000;
+    pi.warp(map, 0);
     pi.clearSavedLocation("MULUNG_TC");
 }
