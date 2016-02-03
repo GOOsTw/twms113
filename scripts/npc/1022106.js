b45ff29a106ccfe4417012c427271d97113d411b
Kinesis <bishoptate01@gmail.com>
2016-02-03 18:30:29 +0800

    Mr Wang

--- scripts/npc/1022106.js
+++ scripts/npc/1022106.js
@@ -5,29 +5,32 @@ var maxp = 5;
 
 function action(mode, type, selection) {
     if (mode == 1) {
-        status++;
+	status++;
     } else {
-        if (status <= 1) {
-            cm.safeDispose();
-            return;
-        }
-        status--;
+	if (status <= 1) {
+	    cm.dispose();
+	    return;
+	}
+	status--;
     }
     if (status == 0) {
-        var selStr = "Would you like to go into the Training Center?";
-        for (var i = 0; i < num; i++) {
-            selStr += "\r\n#b#L" + i + "#Training Center " + i + " (" + cm.getPlayerCount(map + i) + "/" + maxp + ")#l#k";
-        }
-        cm.sendSimple(selStr);
+		var selStr = "選擇一個你想要去的培育中心.";
+		for (var i = 0; i < num; i++) {
+			selStr += "\r\n#b#L" + i + "#培訓中心 " + i + " (" + cm.getPlayerCount(map + i) + "/" + maxp + ")#l#k";
+		}
+	cm.sendSimple(selStr);
     } else if (status == 1) {
-        if (selection < 0 || selection >= num) {
+		if (selection < 0 || selection >= num) {
+			cm.dispose();
+		} else if (cm.getPlayer().getLevel >= 20) {
+            cm.sendNext("二十等以後無法使用唷。");
             cm.dispose();
-        } else if (cm.getPlayerCount(map + selection) >= maxp) {
-            cm.sendNext("This training center is full.");
-            status = -1;
-        } else {
-            cm.warp(map + selection, 0);
-            cm.dispose();
-        }
+		} else if (cm.getPlayerCount(map + selection) >= maxp) {
+			cm.sendNext("這個培育中心已經滿人，請稍後再嘗試!");
+			status = -1;
+		} else {
+			cm.warp(map + selection, 0);
+			cm.dispose();
+		}
     }
 }
