eb8f5f0822b9a54736dab3a78b3bcf6a60e2e1c1
Kinesis <bishoptate01@gmail.com>
2016-03-18 23:21:16 +0800

    have fun

--- scripts/npc/9000008.js
+++ scripts/npc/9000008.js
@@ -9,10 +9,12 @@ var selectlog;
 var postrue = 0;
 var seltrue = 0;
 var stars = "";
-var unlock = 9;
+var unlock = 2;
 var unlocklog = new Array("");
 var str_unlocklog = "";
 var num = 0;
+var gg = 0;
+var it = 0;
 
 function start() {
 	a = -1;
@@ -33,7 +35,7 @@ function action(mode, type, selection) {
 	}else if (a == 0){
 		cm.sendNext("找到了找到了……。點下一步給你說明解除密碼的關鍵步驟。")
     }else if (a == 1){
-      cm.sendYesNo(" - 保險庫密碼鎖任務說明：\r\n\r\n　　密碼鎖由四個不重複的數字組成，在對話框內依次選擇正確的數字，密碼鎖就會被打開，密碼鎖的密碼是由0~9所組成，並且四個數字之間不會重複。\r\n\r\n#r#e任務簡介：#n#k\r\n　　開始遊戲後NPC隨機生成密碼，玩家會獲得9次猜對這組密碼的機會，玩家按照提示依次選擇5個數字，組成四位數的密碼，確認之後會得到提示：\r\n\r\n#r★#k表示答案裡有幾個，數字正確且位置正確\r\n#r☆#k表示答案裡有幾個，數字正確但位置不正確\r\n\r\n#r#e注意事項：#n\r\n　如關閉了NPC對話，那麼再次點擊NPC開始遊戲密碼會改變。");
+      cm.sendYesNo(" - 保險庫密碼鎖任務說明：\r\n\r\n　　密碼鎖由四個不重複的數字組成，在對話框內依次選擇正確的數字，密碼鎖就會被打開，密碼鎖的密碼是由0~9所組成，並且四個數字之間不會重複。\r\n\r\n#r#e任務簡介：#n#k\r\n　　開始遊戲後NPC隨機生成密碼，玩家會獲得3次猜對這組密碼的機會，玩家按照提示依次選擇5個數字，組成四位數的密碼，確認之後會得到提示：\r\n\r\n#r★#k表示答案裡有幾個，數字正確且位置正確\r\n#r☆#k表示答案裡有幾個，數字正確但位置不正確\r\n\r\n#r#e注意事項：#n\r\n　如關閉了NPC對話，那麼再次點擊NPC開始遊戲密碼會改變。 \r\n 且如果機會都用光還是回答失敗，那會有小小的懲罰！！ \r\n獎品隨機的~~~");
       for (var i = 0; i<5; i++) 
       {
         passwords[i] = Math.floor(Math.random()*10);
@@ -141,8 +143,14 @@ function action(mode, type, selection) {
       for (var i = 0; i < unlocklog.length; i++)
         str_unlocklog += unlocklog[i];
       if (postrue == 5) {
+		gg = Math.floor(Math.random() * 6)+1;
+		if (gg >= 2) {
+			it = 4000019;
+		} else {
+			it = 5220040;
+		}
         cm.sendNext("恭喜你,開鎖成功!\r\n\r\n您的記錄：\r\n" + str_unlocklog);
-        cm.gainItem(5220040, 1);
+        cm.gainItem(it, 1);
 		cm.worldMessage("『保險箱密碼』：恭喜玩家:" + cm.getChar().getName() + " 使用了"+num+"次成功開啟保險箱密碼看來是一個很強的解鎖達人!!!");
       } else {
         unlock -= 1;
@@ -153,6 +161,9 @@ function action(mode, type, selection) {
     }else if (a == 9){
       if (postrue != 5) {
         cm.sendOk("您的開鎖機會已經用完了~\r\n\r\n正確密碼為：" + passwords + "\r\n您的記錄：\r\n" + str_unlocklog);
+		cm.worldMessage("『保險箱密碼』：恭喜玩家:" + cm.getChar().getName() + " 使用完機會了並失敗開啟保險箱密碼看來給予一個獎勵死亡一次!!!");
+		cm.getPlayer().setHp(0);
+        cm.getPlayer().updateSingleStat(Packages.client.MapleStat.HP, 0);
         cm.dispose();
       } else {
         cm.sendOk("歡迎您再來挑戰~");
