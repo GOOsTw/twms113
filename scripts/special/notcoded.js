e63a69711321987bee3bff60a1f110a5fac21de8
Kinesis <bishoptate01@gmail.com>
2016-03-21 09:01:00 +0800

    fixed notcoded script error and add new mini game npc

--- scripts/special/notcoded.js
+++ scripts/special/notcoded.js
@@ -1,5 +1,6 @@
-function action(mode, type, selection) {
-    cm.sendOk("嗨~ 我是沒工作的NPC.\r\n我的代碼是 #r" + npcid.toString() + "#k." + "您看到這個就表示這個 NPC 沒有工作\r\n如果這是個很重要的 NPC 請聯繫 GM\r\n謝謝您 !");
-    cm.safeDispose();
-
+function start() {
+	var info = "對不起，我並沒有被管理員設置可使用，如果您覺得我應該工作的，那就請您回報給管理員.\r\n";
+		info += "我的ID編號: #r" + cm.getNpc() + "#k ";
+    cm.sendOk(info);
+	cm.dispose();
 }
