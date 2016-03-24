bd79f1822ec567ed62e23503fb02354b1ed4da9f
Kinesis <bishoptate01@gmail.com>
2016-03-24 19:26:02 +0800

    修正上海任務 高級檢索錯誤 一些修改

--- Source/src/server/quest/MapleQuest.java
+++ Source/src/server/quest/MapleQuest.java
@@ -270,6 +270,20 @@ public class MapleQuest implements Serializable {
             } else {
                 NPCScriptManager.getInstance().endQuest(c.getClient(), npc, getId(), true);
             }        
+        } else {
+            for (MapleQuestAction a : startActs) {
+                if (!a.checkEnd(c, null)) { //just in case
+                    return;
+                }
+            }
+            for (MapleQuestAction a : startActs) {
+                a.runStart(c, null);
+            }
+            if (!customend) {
+                forceStart(c, npc, null);
+            } else {
+                NPCScriptManager.getInstance().endQuest(c.getClient(), npc, getId(), true);
+            }
         }
     }
 
