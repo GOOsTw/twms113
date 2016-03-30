102c4a344ba54c086a0439fd50bb365987f0d014
Kinesis <bishoptate01@gmail.com>
2016-03-30 12:45:48 +0800

    fixed fking no code quest

--- scripts/quest/2156.js
+++ scripts/quest/2156.js
@@ -1,13 +1,17 @@
 var status = -1;
 
 function start(mode, type, selection) {
-    qm.sendNext("Thank you so much.");
-    qm.forceCompleteQuest();
-    qm.dispose();
 }
 
 function end(mode, type, selection) {
-    qm.sendNext("Thank you so much.");
-    qm.forceCompleteQuest();
+    if (qm.haveItem(2210006, 1)) {
+        qm.sendNext("任務完成\r\n獎勵:#r經驗值:7500 名聲x3");
+        qm.gainItem(2210006, -1);
+        qm.getPlayer().addFame(3);
+        qm.gainExp(7500);
+        qm.forceCompleteQuest();
+    } else {
+        qm.sendNext("你沒有我需要的東西。");
+    }
     qm.dispose();
 }
