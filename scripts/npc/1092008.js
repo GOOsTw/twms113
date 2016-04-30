4ad36cd73dbb6ec6b2a539ccd1a4cbdf74841ba7
Kinesis <bishoptate01@gmail.com>
2016-04-30 08:21:15 +0800

    add a skill quest

--- scripts/npc/1092008.js
+++ scripts/npc/1092008.js
@@ -1,11 +1,8 @@
-﻿
-/* Author: aaroncsn (MapleSea Like)
-	NPC Name: 		Shulinch
-	Map(s): 		Nautilus: PRactice Grounds(120000104)
-	Description: 		Unknown
-*/
-
-function start() {
-    cm.sendOk("在世界上什麼，你說什麼？");
+﻿function start() {
+	if (cm.getQuestStatus(6410) == 1) {
+		cm.warp(925010000,0);
+	} else {
+		cm.sendOk("在世界上什麼，你說什麼？");
+	}
     cm.dispose();
 }
--- scripts/npc/1095000.js
+++ scripts/npc/1095000.js
@@ -1,4 +1,4 @@
-var status = -1;
+﻿var status = -1;
 
 function action(mode, type, selection) {
     if (mode == 1) {
@@ -10,7 +10,7 @@ function action(mode, type, selection) {
         status--;
     }
     if (status == 0) {
-        cm.sendYesNo("Would you like to head back?");
+        cm.sendYesNo("你想要回去了嗎??");
     } else if (status == 1) {
         cm.warp(120000104);
         cm.dispose();
