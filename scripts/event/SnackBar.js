d5a364892c42e90d65f0bc2ad7c482f4bad0d3ab
Kinesis <bishoptate01@gmail.com>
2016-05-20 13:13:22 +0800

    some change

--- scripts/event/SnackBar.js
+++ scripts/event/SnackBar.js
@@ -1,5 +1,5 @@
-/**
-    鬼娃恰吉 PQ
+﻿/**
+    黑輪王任務 PQ
 */
 
 var minPlayers = 1;
@@ -15,24 +15,24 @@ function monsterValue(eim, mobId) {
 function setup() {
     em.setProperty("state", "1");
 
-    var eim = em.newInstance("Ghostbaby");
+    var eim = em.newInstance("SnackBar");
 
-    var map = eim.setInstanceMap(749050301);
+    var map = eim.setInstanceMap(741020101);
     map.resetFully();
-    eim.startEventTimer(1200000); //20 分
+    eim.startEventTimer(900000); //15 分
 
     return eim;
 }
 
 function scheduledTimeout(eim) {
-    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 229010000 : 229010000);
+    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 741020100 : 741020100);
 
     em.setProperty("state", "0");
 }
 
 function changedMap(eim, player, mapid) {
     switch (mapid) {
-        case 749050301:
+        case 741020101:
             return;
     }
     eim.unregisterPlayer(player);
@@ -43,7 +43,7 @@ function changedMap(eim, player, mapid) {
 }
 
 function playerEntry(eim, player) {
-    var map = em.getMapFactory().getMap(749050301);
+    var map = em.getMapFactory().getMap(741020101);
     player.changeMap(map, map.getPortal(0));
 }
 
@@ -55,7 +55,7 @@ function playerDisconnected(eim, player) {
 
 function leftParty(eim, player) {
     // If only 2 players are left, uncompletable
-    if (eim.disposeIfPlayerBelow(minPlayers, eim.getProperty("cleared") == null ? 229010000 : 229010000)) {
+    if (eim.disposeIfPlayerBelow(minPlayers, eim.getProperty("cleared") == null ? 741020100 : 741020100)) {
         em.setProperty("state", "0");
     } else {
         playerExit(eim, player);
@@ -64,13 +64,13 @@ function leftParty(eim, player) {
 
 function disbandParty(eim) {
     // Boot whole party and end
-    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 229010000 : 229010000);
+    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 741020100 : 741020100);
 
     em.setProperty("state", "0");
 }
 
 function playerExit(eim, player) {
-    var map = em.getMapFactory().getMap(eim.getProperty("cleared") == null ? 229010000 : 229010000);
+    var map = em.getMapFactory().getMap(eim.getProperty("cleared") == null ? 741020100 : 741020100);
 
     eim.unregisterPlayer(player);
     player.changeMap(map, map.getPortal(0));
@@ -81,20 +81,12 @@ function removePlayer(eim, player) {
     eim.unregisterPlayer(player);
 }
 
-function clearPQ(eim) {
-    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 229010000 : 229010000);
+function clearPQ(eim) {}
 
-    em.setProperty("state", "0");
-}
-
-function finish(eim) {
-    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 229010000 : 229010000);
-
-    em.setProperty("state", "0");
-}
+function finish(eim) {}
 
 function timeOut(eim) {
-    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 229010000 : 229010000);
+    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 741020100 : 741020100);
 
     em.setProperty("state", "0");
 }
@@ -102,7 +94,7 @@ function timeOut(eim) {
 function cancelSchedule() {}
 
 function playerDead(eim, player) {
-    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 229010000 : 229010000);
+    eim.disposeIfPlayerBelow(100, eim.getProperty("cleared") == null ? 741020100 : 741020100);
     em.setProperty("state", "0");
 }
 
