a4cd947b1eca06d77c5eb32597b2fedb6a26c663
Kinesis <bishoptate01@gmail.com>
2016-05-28 16:23:41 +0800

    fixed fking quest

--- scripts/portal/magatia_dark0.js
+++ scripts/portal/magatia_dark0.js
@@ -1,4 +1,8 @@
 function enter(pi) {
-    pi.warp(261020700, 0);
-    pi.playPortalSE();
+	if (pi.getQuestStatus(3309) == 1) {
+		pi.warp(926120000, 0);
+	} else {
+		pi.warp(261020700, 0);
+	}
+	pi.playPortalSE();
 }
