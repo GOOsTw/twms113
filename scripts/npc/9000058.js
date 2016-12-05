1efde58220315860c59bb8b0194cbadeb92ddb6b
Weber Tsai <taida957789@gmail.com>
2016-12-05 18:42:46 +0800

    update

--- scripts/npc/9000058.js
+++ scripts/npc/9000058.js
@@ -23,7 +23,7 @@ function action(mode, type, selection) {
         if (!cm.isQuestFinished(29934)) {
             NewPlayer();
         }
-        cm.sendSimple("歡迎來到啾咪谷~\r\n#b#L2#我要打開藍色小箱子#l\r\n#b#L6#我要抽月光寶盒#l");
+        cm.sendSimple("歡迎來到邊緣谷~\r\n#b#L2#我要打開藍色小箱子#l\r\n#b#L6#我要抽月光寶盒#l");
     } else if (status == 1) {
         if (selection == 2) {
             if (cm.haveItem(4031307, 1) == true) {
@@ -43,6 +43,7 @@ function action(mode, type, selection) {
 }
 
 function NewPlayer() {
+
     if (!cm.haveItem(5000007, 1, true, true) && cm.canHold(5000007, 1)) {
         cm.gainPet(5000007, "黑色小豬", 1, 0, 100, 45, 0);
     }
---
+++ scripts/special/playerHelp.js
@@ -0,0 +1,4 @@
+function start() {
+    cm.sendOk(">_<");
+    cm.dispose();
+}
